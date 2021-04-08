08605041c7994914dd2a28727cddc8b35c22e56e
Boris Arturo González Rivera <borisgr04@gmail.com>
2021-04-08 12:06:07 -0500

    se habillita la migracion acoplada a sqlite para pruebas iniciales.

--- Banco.Infrastructure.Data/BancoContext.cs
+++ Banco.Infrastructure.Data/BancoContext.cs
@@ -9,5 +9,9 @@ namespace Banco.Infrastructure.Data
     public class BancoContext:DbContext
     {
         public DbSet<CuentaBancaria> CuentasBancarias { get; set; }//equivale a Repositorios
+
+        //se debe quitar posteriomente para trabajarlo con inyeccion de dependencias. 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+         => options.UseSqlite(@"Data Source=C:\sqlite\blogging.db");
     }
 }
